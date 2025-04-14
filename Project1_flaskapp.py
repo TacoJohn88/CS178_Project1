478ea5f22d783efe98f4091f408c063009356f29
Nathan Oelsner <nathanoelsner@gmail.com>
2025-04-14 13:51:58 -0500

    added Project1_flaskapp

---
+++ Project1_flaskapp.py
@@ -0,0 +1,35 @@
+# author: Nathan Oelsner
+
+from flask import Flask
+from flask import render_template
+import dbCode
+import creds
+
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key' # this is an artifact for using flash displays; 
+                                   # it is required, but you can leave this alone
+
+db_config = {
+    'host': creds.host,
+    'user': creds.user,
+    'password': creds.password,
+    'database': creds.db
+}
+
+@app.route('/')
+def home():
+    return '<h1>Welcome to the Country Viewer</h1><p>Go to <a href="/countries">/countries</a> to see the list.</p>'
+
+
+@app.route('/countries')
+def countries():
+    #try:
+        countries_list = dbCode.execute_query("SELECT Name, Population FROM country LIMIT 10")
+        return render_template('countries.html', countries=countries_list)
+    #except Exception as e:
+        return f"An error occurred: {e}"
+
+# these two lines of code should always be the last in the file
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
---
+++ dbCode.py
@@ -0,0 +1,37 @@
+import pymysql
+import pymysql.cursors
+import creds
+import boto3
+
+
+
+
+
+def get_conn():
+    """
+    Establish and return a connection to the MySQL RDS database
+    using credentials from creds.py.
+    Uses DictCursor to return query results as dictionaries.
+    """
+    return pymysql.connect(
+        host=creds.host,
+        user=creds.user,
+        password=creds.password,
+        db=creds.db,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+def execute_query(query, args=()):
+    """
+    Execute a SQL query using a connection to RDS.
+    Closes the connection automatically after running the query.
+    Returns the results as a list of dictionaries.
+    """
+    conn = get_conn()
+    try:
+        with conn.cursor() as cur:
+            cur.execute(query, args)
+            rows = cur.fetchall()
+        return rows
+    finally:
+        conn.close()
