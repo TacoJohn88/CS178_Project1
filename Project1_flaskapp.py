efbeb6541568da136dd7e271afb6866b67ed2a36
Nathan Oelsner <nathanoelsner@gmail.com>
2025-04-15 18:20:21 -0500

    updated Project1_flaskapp

--- Project1_flaskapp.py
+++ Project1_flaskapp.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from flask import render_template
+from flask import request
 import dbCode
 import creds
 
@@ -19,14 +20,37 @@ db_config = {
 
 @app.route('/')
 def home():
-    return '<h1>Welcome to the Country Viewer</h1><p>Go to <a href="/countries">/countries</a> to see the list.</p>'
-
+    return ('<h1>Welcome to the Country Viewer</h1>'
+    '<p>Go to <a href="/countries">/countries</a> to see the list.</p>'
+    )
 
 @app.route('/countries')
 def countries():
     try:
-        countries_list = dbCode.execute_query("SELECT Name, Population FROM country LIMIT 10")
-        return render_template('countries.html', countries=countries_list)
+        all_countries = dbCode.execute_query("SELECT * FROM country")
+        return render_template('countries.html', countries=all_countries)
+    except Exception as e:
+        return f"An error occurred: {e}"
+    
+
+@app.route('/popsearch', methods=['GET'])
+def show_pop_form():
+    return render_template('popsearch.html')
+
+
+@app.route('/largepop', methods=['POST'])
+def popsearch():
+    try:
+        pop = request.form.get('pop', type=int)
+
+        if pop is None:
+            return "Invalid population input."
+
+        query = "SELECT * FROM country WHERE Population >= %s ORDER BY Population DESC"
+        largepop = dbCode.execute_query(query, (pop,))
+
+        return render_template('largepop.html', countries=largepop, pop=pop)
+
     except Exception as e:
         return f"An error occurred: {e}"
 
